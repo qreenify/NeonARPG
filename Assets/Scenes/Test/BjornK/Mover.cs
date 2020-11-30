040e51c061818ca2b2c87f735c10fb82ecd414f9
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2020-11-30 20:21:29 +0100

    adds NavMesh and Player and destination point

---
+++ Assets/Scenes/Test/BjornK/Mover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour {
+    [SerializeField] Transform destination;
+    NavMeshAgent navMeshAgent;
+    void Start() 
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+    void Update() 
+    {
+        navMeshAgent.destination = destination.position;
+    }
+}
