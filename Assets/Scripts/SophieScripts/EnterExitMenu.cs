6e5152d37c004940a4c0cbb8bc43d432667323d7
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-12-01 13:16:42 +0100

    Polished PauseGame
    Edited and polished the script PauseGame

--- Assets/Scripts/SophieScripts/EnterExitMenu.cs
+++ Assets/Scripts/SophieScripts/EnterExitMenu.cs
@@ -2,15 +2,8 @@
 
 public class EnterExitMenu : MonoBehaviour
 {
-    PauseGame pauseGame;
-
-    void Awake()
-    {
-        pauseGame = GetComponent<PauseGame>();
-    }
     public void ToggleActive()
     {
         gameObject.SetActive(!gameObject.activeSelf);
-        pauseGame.PauseObjectActivated();
     }
 }
