c4bdc10cbe81bc202e282eb2ad48f848d244ef39
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-12-01 11:57:00 +0100

    Created PauseGame
    With the script PauseGame, the game pauses when the menu is active.

---
+++ Assets/Scripts/SophieScripts/PauseGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    public void PauseObjectActivated()
+    {
+        if (gameObject.activeSelf)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
