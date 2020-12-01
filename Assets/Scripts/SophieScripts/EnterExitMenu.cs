1e7eb16c288597f4847ee315e63b90bdcaf0e286
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-12-01 10:48:03 +0100

    Added Application Quit
    Added Application Quit. If game is running, and player press exit, the application quits.

--- Assets/Scripts/SophieScripts/EnterExitMenu.cs
+++ Assets/Scripts/SophieScripts/EnterExitMenu.cs
@@ -5,40 +5,6 @@ public class EnterExitMenu : MonoBehaviour
     public GameObject menuObject;
     public bool menuActivated;
 
-    void Start()
-    {
-        menuActivated = false;    
-    }
-
-
-    void Update()
-    {
-        //MenuActive();
-    }
-    public void OnClickEnterMenu()
-    {
-        
-        menuActivated = true;
-    }
-
-    public void OnClickExitMenu()
-    {
-        menuActivated = false;
-    }
-
-    public void MenuActive()
-    {
-        if (menuActivated)
-        {
-            menuObject.SetActive(true);
-            ToggleActive();
-        }
-        else
-        {
-            menuObject.SetActive(false);
-        }
-    }
-
     public void ToggleActive()
     {
         gameObject.SetActive(!gameObject.activeSelf);
