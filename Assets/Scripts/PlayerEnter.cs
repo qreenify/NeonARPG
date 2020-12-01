5d17ce29a38eb5443b9c416c1bec014a67e61f0b
Jackoberto <jackoberto01@gmail.com>
2020-12-01 11:36:49 +0100

    Adds Portal To Change Scene

---
+++ Assets/Scripts/PlayerEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerEnter : MonoBehaviour
+{
+    public UnityEvent playerEnterEvent;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerEnterEvent.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    public void LoadScene(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
