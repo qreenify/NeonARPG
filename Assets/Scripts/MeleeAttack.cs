1bc34c93856c76c0d549d9d92254011e5dff5e17
Jackoberto <jackoberto01@gmail.com>
2020-12-02 18:15:36 +0100

    Adds Player Attacks

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -5,10 +5,28 @@ public class MeleeAttack : MonoBehaviour
     public float damage;
     public float range;
     public float timeBetweenAttacks;
+    private float _currentCooldown;
 
-    private void Attack(Transform target)
+    public bool ReadyToAttack => _currentCooldown < 0;
+
+    private void Update()
     {
+        _currentCooldown -= Time.deltaTime;
     }
-    
+
+    public void Attack(GameObject target)
+    {
+        if (InRange(target.transform) && ReadyToAttack)
+        {
+            target.GetComponent<Health>().TakeDamage(damage);
+            _currentCooldown = timeBetweenAttacks;
+        }
+    }
     private bool InRange(Transform target) => Vector3.Distance(target.position, transform.position) < range;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(this.transform.position, range);
+    }
 }
---
+++ Assets/Scripts/PlayerAttackSelector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerAttackSelector : MonoBehaviour
+{
+    Camera _camera;
+    public bool ranged;
+
+    private void Start()
+    {
+        _camera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (!ranged)
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            if (!Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out var hit)) return;
+            if (!hit.collider.gameObject.CompareTag("Enemy")) return;
+            var enemy = hit.collider.gameObject;
+            GetComponent<MeleeAttack>().Attack(enemy);
+        }
+        else
+        {
+            if (!Input.GetMouseButtonDown(0)) return;
+            if (!Physics.Raycast(_camera.ScreenPointToRay(Input.mousePosition), out var hit)) return;
+            GetComponent<PlayerRanged>().TrySpawnProjectile(hit.point);
+        }
+    }
+}
