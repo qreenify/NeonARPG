49af99a128bb361dbc20cf45bdb92cabe59040ea
AlchemistWolf <tobias_lundin@yahoo.com>
2020-12-02 16:05:22 +0100

    camera movement

---
+++ Assets/Scripts/TobiasScript/CameraFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+    // Variables
+    public Transform player;
+    public float smooth = 0.3f;
+
+    public float height;
+
+    private Vector3 velocity = Vector3.zero;
+
+
+
+    //Methods
+
+    void Update()
+    {
+
+        Vector3 pos = new Vector3(player.position.x - 10f, player.position.y + height, player.position.z - 10f);
+        
+        transform.position = Vector3.SmoothDamp(transform.position, pos, ref velocity, smooth);
+        
+    }
+
+
+}
