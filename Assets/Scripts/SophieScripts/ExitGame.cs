c4ef167979b82f63d256fa0e8d9ef45275f37799
Jackoberto <jackoberto01@gmail.com>
2020-12-02 16:17:00 +0100

    Adds main menu to hud

--- Assets/Scripts/SophieScripts/ExitGame.cs
+++ Assets/Scripts/SophieScripts/ExitGame.cs
@@ -5,5 +5,9 @@ public class ExitGame : MonoBehaviour
     public void Quit()
     {
         Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
