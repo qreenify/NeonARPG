da4509087169bd8e18a755384be8a1adc4948a4b
Jackoberto <jackoberto01@gmail.com>
2020-12-02 12:33:31 +0100

    Changes portal to work with each other

--- Assets/Scripts/TrackingEnemy.cs
+++ Assets/Scripts/TrackingEnemy.cs
@@ -4,18 +4,21 @@ using UnityEngine.AI;
 public class TrackingEnemy : MonoBehaviour
 {
     public float trackingRange;
-    public GameObject player;
+    private GameObject _player;
     private NavMeshAgent _agent;
     public float chaseSpeed;
     public float coolDown;
     private float _coolDown;
     private bool _coolDownFinished;
+    private int _inRange;
 
-    private bool InRange => Vector3.Distance(player.transform.position, transform.position) < trackingRange;
+    //private bool InRange => Vector3.Distance(_player.transform.position, transform.position) < trackingRange;
+    private bool InRange => _inRange > 0;
 
     private void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        GetComponent<SphereCollider>().radius = trackingRange;
     }
 
     private void Update()
@@ -48,12 +51,29 @@ public class TrackingEnemy : MonoBehaviour
 
     void Chase()
     {
-        _agent.destination = player.transform.position;
+        _agent.destination = _player.transform.position;
         _agent.speed = chaseSpeed;
     }
 
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position, trackingRange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _inRange++;
+            _player = other.gameObject;
+        }
+    }
+    
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _inRange--;
+        }
     }
 }
