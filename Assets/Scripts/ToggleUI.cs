b69da219aef9eb7bcf9be45b8005e63623e662de
BjornKlarstrom <bjorn.klarstrom@gmail.com>
2020-12-03 21:50:42 +0100

    adds a base UI Prefab with toggle script and a inventory menu

---
+++ Assets/Scripts/ToggleUI.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ToggleUI : MonoBehaviour 
+{
+    [Header("Inventory Toggle")]
+    [SerializeField] KeyCode toggleKeyInventory = KeyCode.I;
+    [SerializeField] KeyCode altToggleKeyInventory = KeyCode.B;
+    [SerializeField] GameObject rootCanvas = null;
+
+    void Awake() 
+    {
+        rootCanvas.SetActive(false);
+    }
+
+    void Update() 
+    {
+        if (Input.GetKeyDown(toggleKeyInventory) || Input.GetKeyDown(altToggleKeyInventory)) 
+        {
+            rootCanvas.SetActive(!rootCanvas.activeSelf);      
+        }
+    }
+}
