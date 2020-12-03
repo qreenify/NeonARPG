dea4c4207a582497941ddf2b81ef35cf7227a996
AlchemistWolf <tobias_lundin@yahoo.com>
2020-12-03 11:58:57 +0100

    camera can now zoom and snap to different targets

---
+++ Assets/Scripts/TobiasScript/CameraFollow.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [Header("Target")]
+    public Transform target;
+
+    [Header("Distances")]
+    [Range(2f,20f)]
+    public float distance = 5f;
+    public float minDistance = 1f;
+    public float maxDistance = 7f;
+
+    public Vector3 offset;
+    [Header("Speeds")]
+    public float smoothSpeed = 5f;
+    public float scrollSensitivity = 1;
+       
+
+    void Start()
+    {
+        
+
+
+    }
+
+    private void LateUpdate()
+    {
+        if (!target)
+        {
+            print("NO TARGET FOR CAMERA");
+                return;
+
+        }
+
+        float num = Input.GetAxis("Mouse ScrollWheel");
+        distance -= num * scrollSensitivity;
+        distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+        Vector3 pos = target.position + offset;
+        pos -= transform.forward * distance;
+
+        transform.position = Vector3.Lerp(transform.position, pos, smoothSpeed * Time.deltaTime);
+
+    }
+
+}
