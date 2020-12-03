b7f44d565eb89e3dc8016c29bc9b995370717ce9
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-12-03 12:40:14 +0100

    adds UnitScript

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Unit
+{
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class Unit : MonoBehaviour
+    {
+        public List<UnitAction> possibleActions = new List<UnitAction>();
+        public UnitAction currentAction;
+
+        private NavMeshAgent navMeshAgent;
+
+        public Transform target;
+
+        public float interactionRange;
+
+        private void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+            foreach(UnitAction action in GetComponents<UnitAction>())
+            {
+                possibleActions.Add(action);
+                action.unit = this;
+            }
+        }
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                //Queue move + Melee attack...
+            }
+
+            if (currentAction == null)
+            {
+                SetCurrentAction();
+            }
+            else if(!currentAction.DoUpdate())
+            {
+                currentAction.Exit();
+                currentAction = null;
+            }
+        }
+
+        void SetCurrentAction()
+        {
+            foreach (UnitAction action in possibleActions)
+            {
+                if (action.IsPossible())
+                {
+                    currentAction = action;
+                    currentAction.Enter();
+                    break;
+                }
+            }
+        }
+
+        public bool InRange(Vector3 position)
+        {
+            return Vector3.Distance(transform.position, position) < interactionRange;
+        }
+        public void MoveTo(Vector3 position)
+        {
+            if(position != navMeshAgent.destination)
+                navMeshAgent.SetDestination(position);
+        }
+        public void StopMove()
+        {
+            navMeshAgent.isStopped = true;
+        }
+        //What's needed:
+        //Melee attack
+        //Ranged attack
+        //In range
+    }
+
+    public abstract class UnitAction : MonoBehaviour
+    {
+        public Unit unit;
+        public abstract bool IsPossible();
+        public abstract bool Enter();
+        public abstract bool DoUpdate();
+        public abstract bool Exit();
+    }
+}
---
+++ Assets/Scripts/UnitMeleeAttack.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Unit
+{
+    [RequireComponent(typeof(Unit))]
+    public class UnitMeleeAttack : UnitAction
+    {
+        public float maxRange = 10;
+        public float range = 2;
+        public float attackDamage = 10;
+
+        public bool InAttackRange
+        {
+            get => Vector3.Distance(transform.position, unit.target.position) < range;
+        }
+        public bool InMaxRange
+        {
+            get => Vector3.Distance(transform.position, unit.target.position) < maxRange;
+        }
+
+        public override bool IsPossible()
+        {
+            if(unit.target == null || !unit.target.gameObject.activeSelf || !InMaxRange)
+            {
+                return false;
+            }
+            return true;
+        }
+        public override bool Enter()
+        {
+            return true;
+        }
+        public override bool DoUpdate()
+        {
+            if (Attack())
+            {
+                return false;
+            }
+            return IsPossible();
+        }
+        public override bool Exit()
+        {
+            return true;
+        }
+        bool Attack()
+        {
+            if (InAttackRange)
+            {
+                unit.StopMove();
+                Debug.Log("Damage!");
+                unit.target.GetComponent<Health>().TakeDamage(attackDamage);
+                return true;
+            }
+            else
+            {
+                unit.MoveTo(unit.target.position);
+                return false;
+            }
+        }
+    }
+}
