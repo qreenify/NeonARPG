dee2eee542b6e50e1b01e0987caa96d14dacb879
Sopuffer <sophie.ahlberg@forsbergsskola.student.se>
2020-12-07 17:24:40 +0100

    Created Autoheal script
    Created simple Autoheal script that can be attached to the player.

---
+++ Assets/Scripts/SophieScripts/AutoHealing.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoHealing : MonoBehaviour
+{
+    public float addHealth = 5;
+    private Health health;
+    public float time;
+    public float healTime;
+
+    private void Start()
+    {
+        health = GetComponent<Health>();
+    }
+    void Update()
+    {
+        if(health.CurrentHealth < health.maxHealth)
+        {
+            time += Time.deltaTime;
+        }
+        else
+        {
+            time = 0.0f;
+        }
+        AutoHeal();
+    }
+
+    public void AutoHeal()
+    {
+        if(time >=healTime)
+        {
+            health.CurrentHealth += addHealth;
+            time -= healTime;
+            Debug.Log(health.CurrentHealth);
+        }
+    }
+}
