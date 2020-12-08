33211b843f80d70ef6fc0730d90ed4d68d196fbc
MartinForsmark <70843057+MartinForsmark@users.noreply.github.com>
2020-12-08 17:13:52 +0100

    Started with DamagePopup in progress

---
+++ Assets/Scripts/AWGscripts/DamagePopup.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DamagePopup : MonoBehaviour
+{
+    public static DamagePopup Create(Vector3 position, int damageAmount, bool isCriticalHit)
+    {
+        Transform damagePopupTransform = Instantiate(GameAssets.i.pfDamagePopup, position, Quaternion.identity);
+        
+        DamagePopup damagePopup = damagePopupTransform.GetComponent<DamagePopup>();
+        damagePopup.Setup(damageAmount, isCriticalHit);
+
+        return damagePopup;
+    }
+
+    private const float Disappear_Timer_Max = 1f;
+    private TextMeshPro textMesh;
+    private float disappearTimer;
+    private Color textColor;
+    
+    public void Awake() {
+        textMesh = transform.GetComponent<TextMeshPro>();
+    }
+
+    public void Setup(int damageAmount, bool isCriticalHit) {
+        textMesh.SetText(damageAmount.ToString());
+        if (!isCriticalHit)
+        {
+            textMesh.fontSize = 36;
+            textColor = Color.yellow;
+        }
+
+        else {
+                textMesh.fontSize = 45;
+                textColor = Color.red;
+        }
+
+        textColor = textMesh.color;
+        disappearTimer = Disappear_Timer_Max;
+    }
+
+    private void Update()
+    {
+        float moveYSpeed = 20f;
+        transform.position += new Vector3(0, moveYSpeed) * Time.deltaTime;
+
+        if (disappearTimer < Disappear_Timer_Max * .5f)
+        {
+            float decreaseScaleAmount = 1.5f;
+            transform.localScale += Vector3.one * decreaseScaleAmount * Time.deltaTime;
+        }
+
+        {
+            float decreaseScaleAmount = 1f;
+            transform.localScale -= Vector3.one * decreaseScaleAmount * Time.deltaTime;
+        }
+
+        disappearTimer -= Time.deltaTime;
+        if (disappearTimer < 0)
+        {
+            float disappearSpeed = 3f;
+            textColor.a -= disappearSpeed * Time.deltaTime;
+            textMesh.color = textColor;
+
+            if (textColor.a < 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
