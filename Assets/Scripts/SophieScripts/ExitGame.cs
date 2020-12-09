017653f1b8d36465179bca2203e51bd4fd3c9ffc
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-12-09 17:23:22 +0100

    fixes Exit time

--- Assets/Scripts/SophieScripts/ExitGame.cs
+++ Assets/Scripts/SophieScripts/ExitGame.cs
@@ -3,14 +3,14 @@
 public class ExitGame : MonoBehaviour
 {
     public float exitTime = 1;
-    public float exitStartTime;
+    float exitStartTime;
     public void Awake()
     {
         enabled = false;
     }
     public void Update()
     {
-        if(Time.time - exitStartTime > exitTime)
+        if(Time.time - exitStartTime < exitTime)
         {
             Application.Quit();
         #if UNITY_EDITOR
@@ -20,7 +20,10 @@ public class ExitGame : MonoBehaviour
     }
     public void Quit()
     {
-        exitStartTime = Time.time;
-        enabled = true;
+        if (!enabled)
+        {
+            exitStartTime = Time.time;
+            enabled = true;
+        }
     }
 }
