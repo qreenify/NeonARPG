5c6bbab1c6fbfd8b8994cda1252dff9e783cbed6
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-12-11 20:57:14 +0100

    adds RotationScript

---
+++ Assets/Scripts/Generic/RotateObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour
+{
+    public float speed;
+    private Vector3 vSpeed;
+    void Start()
+    {
+        vSpeed = new Vector3(Random.Range(-speed, speed), Random.Range(-speed, speed), Random.Range(-speed, speed));
+    }
+
+    void Update()
+    {
+        transform.Rotate(vSpeed * Time.deltaTime);
+    }
+
+    private void OnValidate()
+    {
+        vSpeed = new Vector3(Random.Range(-speed, speed), Random.Range(-speed, speed), Random.Range(-speed, speed));
+    }
+}
