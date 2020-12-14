70d8dc1381721f800ed4b5fed6b6d93f6b96e1db
Jackoberto <jackoberto01@gmail.com>
2020-12-14 16:40:41 +0100

    Added SaveState Script And GlobalI.txt

--- Assets/Scripts/Generic/SaveState.cs
+++ Assets/Scripts/Generic/SaveState.cs
@@ -1,17 +1,50 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+using System.IO;
+using System.Linq;
 
 public class SaveState : MonoBehaviour
 {
+    [SerializeField] private int id;
+    private static List<SaveState> _savedStates = new List<SaveState>();
     private int Collected
     { 
-        get => PlayerPrefs.GetInt($"state_{GetInstanceID()}", 0);
-        set => PlayerPrefs.SetInt($"state_{GetInstanceID()}", value);
+        get => PlayerPrefs.GetInt($"state_{SceneManager.GetActiveScene().name}_{id}", 0);
+        set => PlayerPrefs.SetInt($"state_{SceneManager.GetActiveScene().name}_{id}", value);
+    }
+
+    private static int GlobalId
+    {
+        get => GetGlobalId();
+        set => SaveGlobalId(value);
+    }
+
+    static void SaveGlobalId(int value)
+    {
+        var path = $"{Application.dataPath}/EditorData/GlobalId.txt";
+        if (!File.Exists(path))
+        {
+            Debug.LogError($"GlobalId File Does Not Exist At {path}");
+        }
+        File.WriteAllText(path, value.ToString());
+    }
+    
+    static int GetGlobalId()
+    {
+        var path = $"{Application.dataPath}/EditorData/GlobalId.txt";
+        if (!File.Exists(path))
+        {
+            Debug.LogError($"GlobalId File Does Not Exist At {path}");
+        }
+        var lines = File.ReadAllLines(path);
+        return lines.Select(int.Parse).FirstOrDefault();
     }
 
     private void Awake()
     {
+        _savedStates.Add(this);
         Saved();
     }
 
@@ -23,7 +56,7 @@ public class SaveState : MonoBehaviour
         }
     }
 
-    private void SetActive(bool active)
+    public void SetActive(bool active)
     {
         var renderers = GetComponents<Renderer>();
         var colliders = GetComponents<Collider>();
@@ -37,14 +70,35 @@ public class SaveState : MonoBehaviour
         }
     }
 
-    public void SetCollected()
+    public static void RemoveAllSavedStates()
     {
-        Collected = (int) CollectedState.Collected;
+        foreach (var states in _savedStates)
+        {
+            states.SetCollected(CollectedState.NotCollected);
+        }
     }
-    
-    private enum CollectedState
+
+    public void SetCollected(bool state) => Collected = state ? 1 : 0;
+    private void SetCollected(CollectedState state) => Collected = (int) state;
+
+    private void OnValidate()
     {
-        NotCollected,
-        Collected
+        if (gameObject.scene.name == null) return;
+        if (id == 0)
+        {
+            id = GlobalId += 1;
+        }
     }
+
+    [ContextMenu("ResetID")]
+    private void ResetID()
+    {
+        id = 0;
+    }
+}
+
+public enum CollectedState
+{
+    NotCollected,
+    Collected
 }
