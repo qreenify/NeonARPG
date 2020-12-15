74bd47ff2aa66847289682d8c3b70023b53a19d9
DesignerJackJarvis <DesignerJackJarvis@users.noreply.github.com>
2020-12-15 17:25:16 +0100

    Transition lvl 3 changes

---
+++ Assets/Scripts/SophieScripts/FeedbackDisplays.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class FeedbackDisplays : MonoBehaviour
+{
+    Renderer[] myMaterials;
+    public float fadeTime = 2f;
+    public float fadeLevelMultiplier = 1;
+    public float fadeDamageMultiplier = 1;
+    [ColorUsage(true, true)]
+    public Color levelColor;
+    [ColorUsage(true, true)]
+    public Color damageColor;
+
+
+    float Amount = 0f;
+  
+    private void Awake()
+    {
+        
+        myMaterials = GetComponentsInChildren<Renderer>();
+    }
+
+    private void Update()
+    {
+        if (Amount > 0)
+        {
+            Amount -= Time.deltaTime / fadeTime;
+
+            foreach (var material in myMaterials)
+            {
+                material.material.SetFloat("_DamageAmount", Amount);
+
+            }
+        }
+    }
+    public void DamageFeedback()
+    {
+        Amount = fadeDamageMultiplier;
+
+        foreach (var material in myMaterials)
+        {
+            material.material.SetColor("_PulseColor", damageColor);
+            material.material.SetFloat("_DamageAmount", Amount);
+
+        }
+    }
+
+    public void LevelUpFeedback()
+    {
+        Amount = fadeLevelMultiplier;
+
+        foreach (var material in myMaterials)
+        {
+            material.material.SetColor("_PulseColor", levelColor);
+            material.material.SetFloat("_DamageAmount", Amount);
+
+        }
+    }
+
+}
