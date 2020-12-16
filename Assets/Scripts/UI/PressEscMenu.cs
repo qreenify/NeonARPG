0ab9d0f9c06791b6b3ee2ab604f7e55608a087e8
FelixDauber <71184613+FelixDauber@users.noreply.github.com>
2020-12-16 15:45:45 +0100

    removes hud camera

--- Assets/Scripts/UI/PressEscMenu.cs
+++ Assets/Scripts/UI/PressEscMenu.cs
@@ -1,17 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PressEscMenu : MonoBehaviour
 {
     public KeyCode enterMenuKey;
     public EnterExitMenu enterExitMenu;
-  
+    public bool active;
+    public UnityEvent onActivate;
+    public UnityEvent onDeactivate;
+
+
     void Update()
     {
         if (Input.GetKeyDown(enterMenuKey))
         {
-            enterExitMenu.ToggleActive();
+            ToggleUI();
         }
+    }
+    void ToggleUI()
+    {
+        enterExitMenu.ToggleActive();
+
+        //Make sound
     }
 }
