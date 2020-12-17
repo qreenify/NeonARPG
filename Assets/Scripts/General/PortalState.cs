32b5ed2b344cb13774f00263fa30f0f5b3c26c95
Jackoberto <jackoberto01@gmail.com>
2020-12-17 10:14:17 +0100

    PortalState and small change to shader

---
+++ Assets/Scripts/General/PortalState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PortalState : MonoBehaviour
+{
+    public bool defaultState;
+    private GameObject _portal;
+
+    private bool IsOpen
+    {
+        get
+        {
+            var defaultState = 0;
+            if (this.defaultState)
+            {
+                defaultState = 1;
+            }
+            var state = PlayerPrefs.GetInt(name + _portal.name + "_state", defaultState);
+            return state != 0;
+        }
+        set
+        {
+            var state = 0;
+            if (value)
+            {
+                state = 1;
+            }
+            PlayerPrefs.SetInt(name + _portal.name + "_state", state);
+        }
+    }
+
+    private void Start()
+    {
+        _portal = GetComponentInChildren<Portal>(true).gameObject;
+        _portal.SetActive(IsOpen);
+    }
+
+    private void OnDestroy()
+    {
+        IsOpen = _portal.activeSelf;
+    }
+}
