04dbebe0fbbd1b9ad82543a40b86366993284004
Jackoberto <jackoberto01@gmail.com>
2020-12-18 00:42:06 +0100

    Destroys VFX beam after time

---
+++ Assets/Scripts/Generic/DestroyAfterSeconds.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    public float time = 1f;
+
+    private void Awake()
+    {
+        Destroy(gameObject, time);
+    }
+}
